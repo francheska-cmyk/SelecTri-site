b4da286cd2126773bf2a7035102e193068276b19
francheska-cmyk <charlotfrancheska@gmail.com>
2026-05-26 11:40:30 +0200

    Ajout backcolor nav mobile

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const navMenu = document.querySelector('.nav-menu');
 
 menuOpener.addEventListener('click', () => {
     const isOpen = menuOpener.classList.toggle('open');
-    navMenu.classList.toggle('open');
     menuOpener.setAttribute('aria-expanded', isOpen);
+    console.log(menuOpener);
+    navMenu.classList.toggle('open');
 });
